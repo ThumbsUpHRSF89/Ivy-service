a82c32275d0ba85c2a127f4edfe5003c45a3af35
ivy <zhengyumeng1125@gmail.com>
2018-03-02 20:35:42 -0800

    setup package.json webpack babel eslint

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              root: '.',
+              // url: false,
+            },
+          }],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  output: {
+    filename: './public/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
