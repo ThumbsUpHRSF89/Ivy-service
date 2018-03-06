2da41422991c0b500581c4b8f10012b3f21f11b5
ivy <zhengyumeng1125@gmail.com>
2018-03-05 16:08:01 -0800

    local side product id route working

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const dataController = require('../db/models/products.js');
 
 const port = process.env.PORT || 8001;
 
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
 
 app.get('/product/:id', async (req, res) => {
   const id = req.params;
   let data = await dataController.findProduct(id);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-  res.status(201);
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+  // res.status(201);
   res.send(data);
 });
 
